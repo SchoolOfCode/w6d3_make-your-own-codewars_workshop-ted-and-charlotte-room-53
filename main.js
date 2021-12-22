daa8853530ffe9d77b805aa4c5333255687a467e
ted.phillips <ted.phillips11@btinternet.com>
2021-12-22 16:20:05 +0000

    completed Kata

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 /* 
 👉 Write your kata here!
+
+Take an array of numbers and translate each into its corresponding letter to form a word. 
+If a number in the array is below 1 or the array is empty it should throw an error that says, "This array is not valid.".
+If a number is above 26 it should start from A again and loop around. 
+
+For example: 
+[6, 15, 15, 4] should return food.
+[-2, 4, 20] should return an error.
+[32, 15, 15, 30] should return food.
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+export function getWordFromNumbers(arr){
+    
+    return word
+}
